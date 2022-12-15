43e4ff457ee24e4fa75dfd10330fb70c0184d8ec
muhammad-Osman <johndoe@example.com>
2022-12-15 19:22:41 +0500

    new changes

---
+++ src/main/java/dev/usman/springsecurity/service/AuthService.java
@@ -0,0 +1,5 @@
+package dev.usman.springsecurity.service;
+
+public interface AuthService {
+    
+}
---
+++ src/main/java/dev/usman/springsecurity/service/impl/AuthServiceImpl.java
@@ -0,0 +1,10 @@
+package dev.usman.springsecurity.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import dev.usman.springsecurity.service.AuthService;
+
+@Service
+public class AuthServiceImpl implements AuthService {
+    
+}
