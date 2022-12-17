a433f70ff43fa31e1e10e503b221eb861333faad
muhammad-Osman <muhammad.usman1282@outlook.com>
2022-12-17 15:19:46 +0500

    completed

--- src/main/java/dev/usman/springsecurity/controller/AuthController.java
+++ src/main/java/dev/usman/springsecurity/controller/AuthController.java
@@ -1,5 +1,11 @@
 package dev.usman.springsecurity.controller;
 
+import dev.usman.springsecurity.model.Customer;
+import dev.usman.springsecurity.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/auth")
 public class AuthController {
 
-    
+    @Autowired
+    private AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> registerCustomer(@RequestBody Customer customer) {
+        return authService.addCustomer(customer);
+    }
 
 }
--- src/main/java/dev/usman/springsecurity/service/AuthService.java
+++ src/main/java/dev/usman/springsecurity/service/AuthService.java
@@ -1,5 +1,10 @@
 package dev.usman.springsecurity.service;
 
+import dev.usman.springsecurity.model.Customer;
+import org.springframework.http.ResponseEntity;
+
 public interface AuthService {
+
+    public ResponseEntity addCustomer(Customer customer);
     
 }
